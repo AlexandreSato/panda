6fca4adc36df17858720b0313d039fa56c491674
AlexandreSato <sato_alexandre@hotmail.com>
2023-09-10 11:15:31 -0300

    Unsafe DO NOT use for now, experiences only in controled env

--- board/safety/safety_toyota.h
+++ board/safety/safety_toyota.h
@@ -31,6 +31,7 @@ const CanMsg TOYOTA_TX_MSGS[] = {{0x283, 0, 7}, {0x2E6, 0, 8}, {0x2E7, 0, 8}, {0
                                  {0x128, 1, 6}, {0x141, 1, 4}, {0x160, 1, 8}, {0x161, 1, 7}, {0x470, 1, 4},  // DSU bus 1
                                  {0x2E4, 0, 5}, {0x191, 0, 8}, {0x411, 0, 8}, {0x412, 0, 8}, {0x343, 0, 8}, {0x1D2, 0, 8}, {0x1D3, 0, 8}, // LKAS + ACC
                                  {0x750, 0, 8}, // white list 0x750 for Enhanced Diagnostic Request
+                                 {0x344, 0, 8}, // BrakeHold
                                  {0x200, 0, 6}};  // interceptor
 
 AddrCheckStruct toyota_addr_checks[] = {
@@ -281,7 +282,7 @@ static int toyota_fwd_hook(int bus_num, int addr) {
     // in TSS2, 0x191 is LTA which we need to block to avoid controls collision
     int is_lkas_msg = ((addr == 0x2E4) || (addr == 0x412) || (addr == 0x191));
     // in TSS2 the camera does ACC as well, so filter 0x343
-    int is_acc_msg = (addr == 0x343);
+    int is_acc_msg = (addr == 0x343 || addr == 0x344); // TODO Refactor this 0x344 shit
     int block_msg = is_lkas_msg || (is_acc_msg && !toyota_stock_longitudinal);
     if (!block_msg) {
       bus_fwd = 0;
